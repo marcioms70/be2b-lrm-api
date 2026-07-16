01cb0d507b97721f79c0a8039b66c5f4859ca141
Marcio Mendes <marciomendes36@gmail.com>
2026-07-15 21:49:44 -0400

    register creative generation routes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,4 +2,5 @@ import "./mediaUploadPatch.js";
 import "./instagramLeadPatch.js";
 import "./metaAdsPatch.js";
 import "./metaAdsLivePublishPatch.js";
+import "./creativeGenerationPatch.js";
 import "./server.js";
